e60968ba86674abb76687c3d21227f6ddb9c3af9
NolaZheng <mania001988@gmail.com>
2020-05-18 15:50:49 +0800

    deploy

---
+++ precache-manifest.2c83e19a322f287bf43e115ccc3a0f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb48b90ba3a748026738c21fb441980",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b00caf3e74b6c14eb997",
+    "url": "/static/css/main.612e6b07.chunk.css"
+  },
+  {
+    "revision": "75cbe0a330b5f48e2c9e",
+    "url": "/static/js/2.b9aaece6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b9aaece6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00caf3e74b6c14eb997",
+    "url": "/static/js/main.a39e21a5.chunk.js"
+  },
+  {
+    "revision": "9809d08af12eb88895cf",
+    "url": "/static/js/runtime-main.074e9ac4.js"
+  }
+]);
